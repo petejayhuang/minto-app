d455002aaefe2fdcf8abcb3a78ebf373a96c88af
Pete Huang <petejayhuang@users.noreply.github.com>
2018-06-24 22:26:36 +0100

    build assets for app

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -1,7 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
 import styled from "styled-components"
-import { feedFakeData } from "../fakeData/feed"
 import { Link } from "react-router-dom"
 import LikeButton from "./LikeButton"
 
