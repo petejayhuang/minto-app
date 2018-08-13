6ad8196f4bee52a3713e43a7d503cd706bc1a51b
Pete Huang <petejayhuang@users.noreply.github.com>
2018-08-13 23:36:02 +0100

    server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express()
 app.use(express.static('client/build'))
 
 app.get('*', (req, res) => {
+  console.log('req', req)
   res.sendFile(path, resolve(__dirname, 'client', 'build', 'index.html'))
 })
 
