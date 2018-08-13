b216bc1d8b7ffa65edf6b3984dc074807c05f78a
Pete Huang <petejayhuang@users.noreply.github.com>
2018-08-13 23:40:16 +0100

    server.js 2

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static('client/build'))
 
 app.get('*', (req, res) => {
   console.log('req', req)
-  res.sendFile(path, resolve(__dirname, 'client', 'build', 'index.html'))
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
 })
 
 app.listen(process.env.PORT || 8000, () => {
