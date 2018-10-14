7346dfe36890687331521490be8118499af11432
Pete Huang <petejayhuang@users.noreply.github.com>
2018-10-14 14:01:37 +0100

    update reset verify email logic

--- client/src/routes/VerifyEmail/index.js
+++ client/src/routes/VerifyEmail/index.js
@@ -7,6 +7,7 @@ import { URLS } from '../../config/constants'
 import { printError, printSuccess, redirect } from '../../actions'
 
 import Button from '../../components/Button'
+import TextInput from '../../components/TextInput'
 
 import {
   VERIFY_EMAIL_REQUEST,
@@ -16,6 +17,7 @@ import {
 
 class VerifyEmail extends Component {
   state = {
+    email,
     email_verified: null,
     token: null
   }
@@ -38,11 +40,11 @@ class VerifyEmail extends Component {
     }
   }
 
-  handleClick = async () => {
+  handleSubmit = async () => {
     const { dispatch } = this.props
-    const { token } = this.state
+    const { email } = this.state
     try {
-      await axios.get(`${URLS.SERVER}/users/resend_verify_email/${token}`)
+      await axios.get(`${URLS.SERVER}/users/resend_verify_email/${email}`)
       dispatch(
         printSuccess('Check your inbox! New email verification email sent.')
       )
@@ -54,23 +56,38 @@ class VerifyEmail extends Component {
     }
   }
 
+  handleInputChange = e => this.setState({ email: e.target.value })
+
   renderErrorUI = () => {
     const {
       ui: { loadingLine }
     } = this.props
-
+    const { email } = this.state
     return (
       <Fragment>
         <h1>That didn't work...</h1>
         <div className="mt-3">
-          <div className="d-flex flex-column align-items-center">
+          <form
+            onSubmit={this.handleSubmit}
+            className="d-flex flex-column align-items-center"
+          >
+            <TextInput
+              handleChange={this.handleInputChange}
+              label="Email"
+              name="email"
+              placeholder="e.g. team@minto.app"
+              required
+              type="email"
+              value={email}
+            />
+
             <Button
+              submit
               className="mb-2"
-              onClick={this.handleClick}
               text=" Resend verification email"
               loading={loadingLine}
             />
-          </div>
+          </form>
           <a className="highlighted" href="mailto:hello@minto.app">
             Email our developers
           </a>
